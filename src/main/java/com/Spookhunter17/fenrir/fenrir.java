fde1737a00fccc2ef8254fa8e312ba8eedfbd4ad
Spookhunter17 <redsenior@hotmail.co.uk>
2014-08-21 18:46:06 +0100

    21/08/2014
    This doesnt quite work.... not sure where its needing fixed? not sure
    what its missing/hasnt loaded?

---
+++ src/main/java/com/Spookhunter17/fenrir/fenrir.java
@@ -0,0 +1,31 @@
+package com.Spookhunter17.fenrir;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="fenrir", name="fenrir", version="1.7.10-1.0")
+public class fenrir 
+{
+	@Mod.Instance("fenrir")
+	public static fenrir instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
